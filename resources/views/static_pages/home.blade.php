74037a048c471a422bc9b1eb8bc01bb85e0c59c9
Winters <gltcoder@126.com>
2018-06-27 13:47:53 +0800

    temp admin 20180627

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.default')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在看到的是<a href="https://laravel-china.org/courses/laravel-essential-training-5.1">Laravel 入门教程</a>的示例项目主页
+        </p>
+
+    </div>
 @stop
